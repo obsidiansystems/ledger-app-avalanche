ac3d7463c1fdeb7eb5a37e642288fb51ecfdfa13
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-03-15 18:05:04 +0000

    Add script to generate hashes

--- release.nix
+++ release.nix
@@ -1,10 +1,19 @@
 { runTest ? true, gitDescribe ? "TEST-dirty" }:
 let
   ledger-app = import ./. { inherit runTest gitDescribe; };
-in {
+in rec {
   analysis-nanos = ledger-app.clangAnalysis.s.wallet;
   release-nanos = ledger-app.nano.s.release.all;
   release-nanox = ledger-app.nano.x.release.all;
   debug-build = (import ./. { debug = true; inherit runTest gitDescribe; }).nano.s.release.all;
   usbtool = import ./nix/usbtool.nix {};
+  hashes = ledger-app.pkgs.writeScript "hashes" ''
+     PATH=${ledger-app.pkgs.coreutils}:$PATH
+     MD5=$(md5sum -b ${release-nanos} | awk '{print $1;}')
+     SHA256=$(sha256sum -b ${release-nanos} | awk '{print $1;}')
+     SHA512=$(sha512sum -b ${release-nanos} | awk '{print $1;}')
+     echo "MD5 | $MD5"
+     echo "SHA256 | $SHA256"
+     echo "SHA512 | $SHA512"
+   '';
 }
