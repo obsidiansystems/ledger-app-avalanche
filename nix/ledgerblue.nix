4ac90e2b170fa4de216596feea6a78160e5fcc6c
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-08-02 19:43:13 +0100

    Fix identifier

--- nix/ledgerblue.nix
+++ nix/ledgerblue.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (import ./dep/ledger-platform/thunk.nix + "/dep/nixpkgs") {}, ... }:
 rec {
-  withLedgerblue = (ledger-platform.pkgs.python3.withPackages (ps: with ps; [
+  withLedgerblue = (pkgs.python3.withPackages (ps: with ps; [
     ecpy hidapi pycrypto python-u2flib-host requests ledgerblue pillow pkgs.hidapi protobuf
   ]));
   shell = withLedgerblue.env;
