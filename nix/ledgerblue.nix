31a2782239cffd7c190a64a5a86f28d2a924e355
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-08-02 18:59:00 +0100

    Thread stale nixpkgs references through ledger-platform

--- nix/ledgerblue.nix
+++ nix/ledgerblue.nix
@@ -1,6 +1,6 @@
-{ pkgs ? import ../nix/dep/nixpkgs {}, ... }:
+{  ledger-platform ? import ../nix/dep/ledger-platform {}, ... }:
 rec {
-  withLedgerblue = (pkgs.python3.withPackages (ps: with ps; [
+  withLedgerblue = (ledger-platform.pkgs.python3.withPackages (ps: with ps; [
     ecpy hidapi pycrypto python-u2flib-host requests ledgerblue pillow pkgs.hidapi protobuf
   ]));
   shell = withLedgerblue.env;
