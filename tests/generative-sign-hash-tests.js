4f901c318ebbaf2800d8edf46dc797d75fca1b23
John Ericson <John.Ericson@Obsidian.Systems>
2022-03-06 23:45:09 -0500

    Fix remaining tests

--- tests/generative-sign-hash-tests.js
+++ tests/generative-sign-hash-tests.js
@@ -31,7 +31,7 @@ describe("Sign Hash tests", () => {
           for (ks of sv) {
             const [keySuffix, sig] = ks;
 
-            await flowAccept(this.speculos);
+            //await flowAccept(this.speculos);
             const key = await this.ava.getWalletExtendedPublicKey(account.toString() + "/" + keySuffix);
 
             const recovered = secp256k1.recover(Buffer.from(hash, "hex"), sig.slice(0, 64), sig[64], false);
