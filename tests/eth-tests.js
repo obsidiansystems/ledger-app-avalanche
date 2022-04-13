de13a52dc64ea0f82ec148cb75252da307d07391
Evan Richard <evan@avalabs.org>
2022-04-13 11:21:49 -0400

    Use chunkPrompts to chunkify all prompts throughout eth-tests.

--- tests/eth-tests.js
+++ tests/eth-tests.js
@@ -138,12 +138,12 @@ const testUnrecognizedCalldataTx = (chainId, gasPrice, gasLimit, amountPrompt, a
         data: '0x' + calldata,
     });
 
-    const prompts = [
-          [{header: "Transfer",     body: amountPrompt + " to " + '0x' + address},
-           {header: "Contract Data", body: "Is Present (unsafe)"},
-           {header: "Maximum Fee",   body: fee}],
-          [finalizePrompt]
-    ];
+    const transferPrompt = {header: "Transfer",     body: amountPrompt + " to " + '0x' + address};
+    const dataPrompt = {header: "Contract Data", body: "Is Present (unsafe)"};
+    const maxFeePrompt = {header: "Maximum Fee",   body: fee};
+
+    const prompts = chunkPrompts([transferPrompt, dataPrompt, maxFeePrompt])
+      .concat([[finalizePrompt]]);
 
     await testLegacySigning(this, chainId, prompts, tx);
 };
@@ -236,12 +236,11 @@ describe("Eth app compatibility tests", async function () {
           // s: use the default
       });
 
-      const prompts = [
-          [{header: "Transfer",     body: '0.000004096 nAVAX' + " to " + '0x' + '0102030400000000000000000000000000000002'},
-           {header: "Contract Data", body: "Is Present (unsafe)"},
-           {header: "Maximum Fee",   body: "0.002293452 GWEI"}],
-          [finalizePrompt],
-      ];
+      const transferPrompt = {header: "Transfer",     body: '0.000004096 nAVAX' + " to " + '0x' + '0102030400000000000000000000000000000002'};
+      const dataPrompt = {header: "Contract Data", body: "Is Present (unsafe)"};
+      const maxFeePrompt = {header: "Maximum Fee",   body: "0.002293452 GWEI"};
+      const prompts = chunkPrompts([transferPrompt, dataPrompt, maxFeePrompt])
+        .concat([[finalizePrompt]]);
 
       await testEIP1559Signing(this, chainId, prompts, tx);
     });
@@ -263,11 +262,10 @@ describe("Eth app compatibility tests", async function () {
           // s: use the default
       });
 
-      const prompts = [
-            [{header: "Transfer",     body: '0.000004096 nAVAX' + " to " + '0x' + '0102030400000000000000000000000000000002'},
-             {header: 'Fee',          body: '0.002293452 GWEI' }],
-            [finalizePrompt],
-      ];
+      const transferPrompt = {header: "Transfer",     body: '0.000004096 nAVAX' + " to " + '0x' + '0102030400000000000000000000000000000002'};
+      const feePrompt = {header: "Fee",   body: "0.002293452 GWEI"};
+      const prompts = chunkPrompts([transferPrompt, feePrompt])
+        .concat([[finalizePrompt]]);
 
       await testEIP1559Signing(this, chainId, prompts, tx);
     });
@@ -278,13 +276,12 @@ describe("Eth app compatibility tests", async function () {
     // Collected from a metamask goerli transaction:
     const tx = Buffer.from('02f9018a82a868808506fc23ac008506fc23ac008316e3608080b90170608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea2646970667358221220404e37f487a89a932dca5e77faaf6ca2de3b991f93d230604b1b8daaef64766264736f6c63430008070033c0', 'hex');
 
-    const prompts = [
-        [{ header: 'Contract', body: 'Creation' },
-         { header: 'Gas Limit', body: '1500000' },
-         { header: 'Data', body: '0x608060405234801561001057600080fd5b506101...' },
-         { header: 'Maximum Fee', body: '90000000 GWEI' }],
-        [finalizePrompt],
-      ];
+    const contractCreationPrompt = { header: 'Contract', body: 'Creation' };
+    const gasLimitPrompt = { header: 'Gas Limit', body: '1500000' };
+    const dataPrompt = { header: 'Data', body: '0x608060405234801561001057600080fd5b506101...' };
+    const maxFeePrompt = { header: 'Maximum Fee', body: '90000000 GWEI' };
+    const prompts = chunkPrompts([contractCreationPrompt, gasLimitPrompt, dataPrompt, maxFeePrompt])
+      .concat([[finalizePrompt]]);
     await testEIP1559Signing(this, chainId, prompts, tx);
   });
 
