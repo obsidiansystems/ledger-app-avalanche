f20f37cf74b7bd137eece60678cd951bd3c6fab4
Krishna Menon <krishna.menon@avalabs.org>
2022-05-25 14:28:56 -0400

    p-chain test fix

--- tests/p-chain-tests.js
+++ tests/p-chain-tests.js
@@ -191,7 +191,7 @@ describe("P-chain import and export tests", () => {
 
 });
 describe('Staking tests', async function () {
-  it.only('can sign an add validator transaction', async function () {
+  it('can sign an add validator transaction', async function () {
     const txn = Buffer.from([
       0x00, 0x00,
       0x00, 0x00, 0x00, 0x0c, 0x00, 0x00, 0x30, 0x39,
@@ -286,7 +286,7 @@ describe('Staking tests', async function () {
     await ui.promptsPromise;
   });
 
-  it('can sign an add subnet validator transaction', async function () {
+  it.only('can sign an add subnet validator transaction', async function () {
     const txn = Buffer.from([
       0x00, 0x00,
       0x00, 0x00, 0x00, 0x0d, 0x00, 0x00, 0x30, 0x39,
@@ -353,16 +353,16 @@ describe('Staking tests', async function () {
     const pathPrefix = "44'/9000'/0'";
     const pathSuffixes = ["0/0", "0/1", "100/100"];
     // Need to add headers for SubnetID and Sigindices?
-    const ui = await flowMultiPrompt(this.speculos, [
-      [{header: 'Sign', body: 'Add Subnet Validator'}],
+    const prompts = chunkPrompts([
+       {header: 'Sign', body: 'Add Subnet Validator'}],
       [{header: 'Transfer', body: '3.999 AVAX to local1mg47uqd7stkvqrp57ds7m28txra45u2uzkta8n'}],
       [{header: 'Validator', body: 'NodeID-NFBbbJ4qCmNaCzeW7sxErhvWqvEQMnYcN' }],
       [{header: 'Start time', body: '2020-07-29 22:07:25 UTC' }],
       [{header: 'End time', body: '2020-08-28 21:57:26 UTC' }],
       [{header: 'Total Stake', body: '0.00054321 AVAX' }],
       [{header: 'Address Index', body: '0' }],
-      [{header: 'Fee',body: '0.001 AVAX'}],
-    ]).concat([[finalizePrompt]]);
+      [{header: 'Fee',body: '0.001 AVAX'}
+      ]).concat([[finalizePrompt]]);
     const ui = await flowMultiPrompt(this.speculos, prompts);
     const sigPromise = this.ava.signTransaction(
       BIPPath.fromString(pathPrefix),
