926a1df4eb0f282b4a1f9ffca082b588da267cf4
dbzguy56 <deepakbirdi25@gmail.com>
2022-07-01 22:15:56 +0000

    Add timeout to let speculos fully start

--- tests/hooks.ts
+++ tests/hooks.ts
@@ -1,9 +1,11 @@
 import HidTransport from '@ledgerhq/hw-transport-node-hid';
 import SpeculosTransport from '@ledgerhq/hw-transport-node-speculos';
 import { SpawnOptions, spawn } from 'child_process';
+import Axios from "axios";
 
 let stdoutVal: string = "";
 let stderrVal: string = "";
+const baseUrl = "http://localhost:5000";
 
 const flushStdio = (proc, n) => () => {
   if (proc && proc.stdio[n])
@@ -11,6 +13,9 @@ const flushStdio = (proc, n) => () => {
   else
     return "";
 };
+
+export const getEvents = async (): Promise<Event[]> =>
+  (await Axios.get(`${baseUrl}/events`)).data["events"];
 
 export const mochaHooks = {
   beforeAll: async function (this: Mocha.Context) { // Need 'function' to get 'this'
@@ -28,6 +33,15 @@ export const mochaHooks = {
           '--sdk', '2.1',
         ], speculosProcessOptions);
         console.log("Speculos started");
+
+        while(true) {
+          try {
+            await getEvents();
+            break;
+          } catch(e) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+          }
+        }
       }
     }
     this.flushStdout = flushStdio(this.speculosProcess, 1);
